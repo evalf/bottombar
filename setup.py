e12398eb762b016c35f4de011fd7fb9214b2f7b2
Gertjan van Zwieten <gertjanvanzwieten@evalf.nl>
2020-02-21 11:48:31 +0100

    add setup script

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+  name = 'bottombar',
+  version = '1.0b1',
+  description = 'context manager that prints a status bar at the bottom of a terminal',
+  author = 'Evalf',
+  author_email = 'info@evalf.com',
+  url = 'https://github.com/evalf/bottombar',
+  py_modules = ['bottombar'],
+  license = 'MIT',
+  python_requires = '>=3.3',
+)
